6ea9774d9ecb7c367806c9e1c03af5cb3aa40b1f
nayVoo <124888494+nayVoo@users.noreply.github.com>
2023-02-12 19:29:26 +0200

    Update bhopCSGO.py

--- bhopCSGO.py
+++ bhopCSGO.py
@@ -2,28 +2,27 @@ import pymem
 from pymem import process
 import keyboard
 import time
+#OFFSETS (REMEMBER, THIS MAY NOT WORK BECAUSE EVERY UPDATE OFFSETS CHANGES AND U GET BAN)
+dwLocalPlayer = (0xDEA964) #-->dwLocalPlayer offset 
+dwForceJump = (0x52BBC7C) #-->dwForceJump offset
+m_fFlags = (0x104) #--> m_fFlags offset
 
+pm = pymem.Pymem('csgo.exe') #here we give to pm characteristic
+client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll #then we call client_server.dll
 
-dwLocalPlayer = (0xDEA964)
-dwForceJump = (0x52BBC7C)
-m_fFlags = (0x104)
-
-pm = pymem.Pymem('csgo.exe')
-client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
-
-def BHOP():
+def BHOP(): 
 	while True:
-		if keyboard.is_pressed('end'):
+		if keyboard.is_pressed('end'): #if something went wrong press 'end'
 				exit (0)
 		try:
 			if keyboard.is_pressed('space'):		
-				player = pm.read_int(client + dwLocalPlayer)
-				jump = client + dwForceJump
-				player_state = pm.read_int(player + m_fFlags)
+				player = pm.read_int(client + dwLocalPlayer) #player its you
+				jump = client + dwForceJump # we analize the jump from server acording to our place
+				player_state = pm.read_int(player + m_fFlags) 
 
-				if player_state == 257 or player_state == 263: #pl on ground
+				if player_state == 257 or player_state == 263: #257 ->pl on ground, 263 -> pl crouch
 					pm.write_int(jump, 5)
-					time.sleep(0.1)
+					time.sleep(0.1) #the lower delay, the more chance for lags
 					pm.write_int(jump, 4)
 		except:
 			pass
